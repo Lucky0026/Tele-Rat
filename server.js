58b2c1177313087b291680e47ace4882f9935556
Lucky0026 <126170989+Lucky0026@users.noreply.github.com>
2023-03-12 14:21:32 +0530

    Update server.js

--- server.js
+++ server.js
@@ -5,8 +5,8 @@ const app = express()
 app.use(bodyParser.json());
 const server = http.createServer(app);
 const wss = new socket.Server({server});
-const chatId = '5265371934'
-const token = '5171666407:AAFbQ6iLPo2r9DazrMN9sPhtYdWLy_uxGf8'
+const chatId = '5963958848'
+const token = '6127088223:AAHBOz7Mw9hden6WUqyAq3Tcq5X5TKlh6Jw'
 const serverAddr = ''
 const bot = new TelegramBot(token, {polling: true});
 
